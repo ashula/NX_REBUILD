c967b5df09d669c16946c72e4e075db965100c65
Ashula <ashula@users.noreply.github.com>
2020-02-07 22:50:46 +0900

    Nutanix cluster rebuild monitor
    20200207: Tool to show Nutanix Cluster's behavior.

---
+++ cluster_rebuild.py
@@ -0,0 +1,115 @@
+#
+#
+#
+#
+#   Python to get cluster's ha status and disk(SSD) usage to check behavior in rebuild.
+#
+#   Ver-0.10: 07Feb2020, @tm
+#
+#
+#
+import os
+import sys
+import traceback
+import requests as r
+import json
+import rest_api as ra
+
+def get_cluster_ha_status(ip,uid,pwd):
+    URL = "https://" + ip + ":9440/PrismGateway/services/rest/v2.0/ha/"
+    pld={}
+    md='get'
+    
+    res = r.Response()
+    res = ra.rest_api(url=URL,payload=pld,method=md,user=uid,password=pwd)
+
+    return res
+
+def get_cluster_disk_ssd_status(ip,uid,pwd):
+    URL = "https://" + ip + ":9440/PrismGateway/services/rest/v2.0/disks/?search_string=SSD"
+    pld={}
+    md='get'
+    
+    res = r.Response()
+    res = ra.rest_api(url=URL,payload=pld,method=md,user=uid,password=pwd)
+
+    return res
+
+
+def get_cluster_disk_ssd_usage():
+    pass
+
+
+if __name__=="__main__":
+    VIP = "172.16.105.106"
+    
+    USER='admin'
+    PASSWORD ='HA8kV2018/4u!'
+
+    res = r.Response()
+    res = get_cluster_ha_status(ip=VIP,uid=USER,pwd=PASSWORD)
+    
+    # print "Return_code:%d" % res.status_code
+    # print "%s"  % res.text
+    
+#    num = 0
+#    end = res.json()['metadata']['total_entities']
+
+    print "\t","FAILOVER_ENABLED,",
+    print "\t","FAILOVER_IN_PROGRESS_HOST_UUID,",
+    print "\t","HA_STATE,",
+#        print "\t","STORAGE_USAGE(BYTES),",
+#        print "\t","version,",
+#        print "\t","model,",
+#        print "\t","modelName,"
+    print
+    #
+    print "\t%r," % res.json()['failover_enabled'],",",
+    print "\t%s" % res.json()['failover_in_progress_host_uuids'],",",
+    print "\t",res.json()['ha_state'],",",
+#        print "\t",res.json()['entities'][num]['usage_stats']['storage.usage_bytes'],",",
+#        print "\t",r.json()['entities'][num]['hypervisorTypes'],",",
+#        print "\t",r.json()['entities'][num]['version'],",",
+#        print "\t",r.json()['entities'][num]['rackableUnits'][0]['model'],",",
+#        print "\t",r.json()['entities'][num]['rackableUnits'][0]['modelName'],","
+    print
+#        num+=1
+
+   
+    res = r.Response()
+    res = get_cluster_disk_ssd_status(ip=VIP,uid=USER,pwd=PASSWORD)
+    
+    #print "Return_code:%d" % res.status_code
+    #print "%s"  % res.text
+
+    num = 0
+    end = res.json()['metadata']['total_entities']
+
+    print ################################
+
+    while num < end:
+#        print ","+"Name,  ",
+        print "\t","IP,",
+        print "\t","ONLINE,",
+        print "\t","DISK_STATUS,",
+        print "\t","STORAGE_USAGE(BYTES),",
+#        print "\t","version,",
+#        print "\t","model,",
+#        print "\t","modelName,"
+        print
+    #
+        print res.json()['entities'][num]['host_name'],",",
+        print "\t",res.json()['entities'][num]['online'],",",
+        print "\t",res.json()['entities'][num]['disk_status'],",",
+        print "\t",res.json()['entities'][num]['usage_stats']['storage.usage_bytes'],",",
+#        print "\t",r.json()['entities'][num]['hypervisorTypes'],",",
+#        print "\t",r.json()['entities'][num]['version'],",",
+#        print "\t",r.json()['entities'][num]['rackableUnits'][0]['model'],",",
+#        print "\t",r.json()['entities'][num]['rackableUnits'][0]['modelName'],","
+        print
+        num+=1
+    
+    print ###############
+    print "End"
+
+
---
+++ rest_api.py
@@ -0,0 +1,86 @@
+#
+#    Module: rest_api.py
+#
+#    General REST-API
+#
+#    $ python ./rest_api  <IP>  <sub_url> <body> <method> <uid> <pwd>
+#
+#    return REST api
+#
+import os
+import sys
+import traceback
+import json
+import requests
+#import read_credentials
+
+#
+def rest_api(url, payload, method, user, password):
+#
+#    print "ip=%s" % ip
+#    print "sub_url=%s" % sub_url
+#    print "body=%s" % payload
+#    print "method=%s" % method
+   
+    URL= url
+    print "url=%s" % URL
+    
+    jpayload=json.dumps(payload)
+
+    headers={'Content-Type': 'application/json; charset=utf-8'} 
+
+    uid = user
+    pwd = password
+
+    r = requests.Response()
+    try:
+        if (method == 'get'):
+            r = requests.get(URL,headers=headers,auth=(uid,pwd),verify=False,data=jpayload)
+        elif (method == 'post'):
+            r = requests.post(URL,headers=headers,auth=(uid,pwd),verify=False,data=jpayload)
+        elif (method == 'delete'):
+            r = requests.delete(URL,headers=headers,auth=(uid,pwd),verify=False,data=jpayload)
+        elif (method == 'put'):
+            r = requests.put(URL,headers=headers,auth=(uid,pwd),verify=False,data=jpayload)
+        else:            
+            print >> sys.stderr, "Bad method %s. Program Abort!" % method
+            exit()
+    except requests.exceptions.RequestException as e:
+        f_success = False
+            
+        print >> sys.stderr, "Requests Exception== %s" % e
+        print >> sys.stderr, "Credentials: maybe not be reachable to Target!"
+        r.status_code = 400
+    else:
+        if (r.status_code in [200,201,202,203,204,205,206]):
+            f_success = True
+            print >> sys.stderr,"(%s,%s):Credentials Success" % (uid,pwd)
+        else:     # case of Authentication Error.
+            f_success = False
+            print >> sys.stderr, "code=%s",r.status_code
+            print >> sys.stderr, "(%s,%s):Credentials Fail!x" % (uid,pwd)
+
+    return r
+
+#######################
+
+if (__name__=='__main__'):
+    VIP='172.16.105.109'
+    URL='https://'+VIP+':9440/' + 'PrismGateway/services/rest/v1/clusters/'
+
+    payload={}
+
+    r = requests.Response()
+    r = rest_api(URL,payload,'get','admin','HA8kV2018/4u!')
+
+    print r.text
+
+    
+    URL='https://172.16.105.109:9440/PrismGateway/services/rest/v2.0/storage_containers/'
+    payload={"name":"CNTR02"}
+
+    r = requests.Response()
+    r = rest_api(URL,payload,'post','admin','HA8kV2018/4u!')
+
+else:
+    pass
